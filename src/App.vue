d21272cc922a1d6563c4c7091bbeaf7cd8d377c3
Néstor Pérez López <nestorpl006@gmail.com>
2026-05-13 09:10:53 +0200

    UTER-1: dibujar el tablero de 3x3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="juego">
+    <h1>Tres en Raya</h1>
+    <div class="tablero">
+      <div class="celda" v-for="n in 9" :key="n"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.juego {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Arial, sans-serif;
+}
+.tablero {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  gap: 5px;
+  background-color: black;
+  padding: 5px;
+}
+.celda {
+  width: 100px;
+  height: 100px;
+  background-color: white;
+}
+</style>
+s
